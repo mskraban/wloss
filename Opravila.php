48e2860fe1a6dd5e65998d9772c92cd633cb89f6
mario <mario.skraban@gmail.com>
2018-08-05 02:01:47 +0200

    Added Chores list

--- Opravila.php
+++ Opravila.php
@@ -127,8 +127,94 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 
             <div class="row">
                 <div class="col-12 col-lg-12 col-xl-12 widget">
-                    <div class="widget-task bg-purp text-white">
-                        Vsa opravila
+                    <div class="widget-auto bg-purp text-white">
+                        <div class="row">
+                            <div class="col-12 task-first">Seznam opravil</div>
+                            <div class="col-12 divider"></div>
+                            <div class="col-12 task">
+                                <div class="col-2"><i class="far fa-square"></i></div>
+                                <div class="col-2">Kuhanje</div>
+                                <div class="col-3">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <div class="col-3">Visoka prioriteta</div>
+                                <div class="col-2">Aktivno</div>
+                            </div>
+                            <div class="col-12 divider"></div>
+                            <div class="col-12 task">
+                                <div class="col-2"><i class="far fa-square"></i></div>
+                                <div class="col-2">Kuhanje</div>
+                                <div class="col-3">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <div class="col-3">Visoka prioriteta</div>
+                                <div class="col-2">Aktivno</div>
+                            </div>
+                            <div class="col-12 divider"></div>
+                            <div class="col-12 task">
+                                <div class="col-2"><i class="far fa-square"></i></div>
+                                <div class="col-2">Kuhanje</div>
+                                <div class="col-3">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <div class="col-3">Visoka prioriteta</div>
+                                <div class="col-2">Aktivno</div>
+                            </div>
+                            <div class="col-12 divider"></div>
+                            <div class="col-12 task">
+                                <div class="col-2"><i class="far fa-square"></i></div>
+                                <div class="col-2">Kuhanje</div>
+                                <div class="col-3">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <div class="col-3">Visoka prioriteta</div>
+                                <div class="col-2">Aktivno</div>
+                            </div>
+                            <div class="col-12 divider"></div>
+                            <div class="col-12 task">
+                                <div class="col-2"><i class="far fa-square"></i></div>
+                                <div class="col-2">Kuhanje</div>
+                                <div class="col-3">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <div class="col-3">Visoka prioriteta</div>
+                                <div class="col-2">Aktivno</div>
+                            </div>
+                            <div class="col-12 divider"></div>
+                            <div class="col-12 task">
+                                <div class="col-2"><i class="far fa-square"></i></div>
+                                <div class="col-2">Kuhanje</div>
+                                <div class="col-3">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <div class="col-3">Visoka prioriteta</div>
+                                <div class="col-2">Aktivno</div>
+                            </div>
+                            <div class="col-12 divider"></div>
+                            <div class="col-12 task">
+                                <div class="col-2"><i class="far fa-square"></i></div>
+                                <div class="col-2">Kuhanje</div>
+                                <div class="col-3">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <div class="col-3">Visoka prioriteta</div>
+                                <div class="col-2">Aktivno</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
